20f2f979e663e32536267d5fbdeee4c81490498a
Christian Mc Carthy <cmccarthy@pivotal.io>
2019-02-25 11:55:38 +0000

    Add main.go to git ignore

--- queen-attack/main.go
+++ queen-attack/main.go
@@ -1,1 +1,14 @@
-package queen_attack
+package main
+
+import (
+	"./queenattack"
+	"fmt"
+)
+func main() {
+
+	attack, err := queenattack.CanQueenAttack("d1", "g4")
+
+	fmt.Println(attack, err)
+
+
+}
