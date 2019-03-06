d06f2f673138953ec2d6f3252fa5c8161c4917a8
Christian Mc Carthy <cmccarthy@pivotal.io>
2019-03-06 08:41:05 +0000

    Wokring Fine and Passing Tests.

--- connect_hex_game/connect/connect.go
+++ connect_hex_game/connect/connect.go
@@ -34,14 +34,6 @@ func NewTile(x int, y int, value rune) *Tile {
 	return t
 }
 
-func MakeTile(x int, y int, value rune) Tile {
-
-	 t:= Tile {x, y, false, value, 0,
-					nil, nil, nil, nil }
-
-	 return t
-}
-
 
 func (t Tile) printTile() {
 
@@ -106,8 +98,8 @@ func NewBoard (sboard []string) *Board{
 
 	b := new(Board)
 	b.tiles = make([]*Tile, len(sboard)*len(sboard[0]) )
-	b.height = len(sboard)
 	b.width = len(sboard[0])
+	b.height = len(sboard)
 
 
 	// want to iterate over the sboard []string and create tile for each character
@@ -131,11 +123,11 @@ func (b Board) printBoard() {
 
 func (b Board) returnTile(x int, y int) *Tile{
 
-	if x > b.height || x < 0 {
+	if x > b.width || x < 0 {
 		return nil
 	}
 
-	if y > b.width || y < 0 {
+	if y > b.height || y < 0 {
 		return nil
 	}
 
@@ -160,7 +152,7 @@ func initGraph (tboard *Board) {
 		i := t.row
 		j := t.col
 
-		fmt.Printf("Tile [%d, %d], Value %s\n", i, j, string(t.value))
+		//fmt.Printf("Tile [%d, %d], Value %s\n", i, j, string(t.value))
 
 		connected := [6]*Tile {
 			tboard.returnTile(i,j - 1),
@@ -173,10 +165,10 @@ func initGraph (tboard *Board) {
 
 		for _, c := range connected {
 			if c != nil {
-				fmt.Printf("	Connected Tile [%d, %d], Value %s\n", c.row, c.col, string(c.value))
+				//fmt.Printf("	Connected Tile [%d, %d], Value %s\n", c.row, c.col, string(c.value))
 
 				if t.value == c.value {
-					fmt.Print("		Same Value, Adding Edge\n")
+					//fmt.Print("		Same Value, Adding Edge\n")
 					t.edgeCount++
 					newedge:= NewEdge(t, c)
 					if t.headP == nil {
@@ -197,11 +189,117 @@ func initGraph (tboard *Board) {
 
 }
 
+func (b Board) checkWinner () string {
+
+	// first start with checking X
+	// X goes left to right.
+	// make a queue of
+
+	Xwin := checkX(b)
+
+	Owin := checkY(b)
+
+	if Xwin {
+		return "X"
+	} else if Owin {
+		return "O"
+	} else {
+		return ""
+	}
+}
+
+func checkX (b Board) (winner bool) {
+
+	// left to right
+
+	var queue []*Tile
+
+	for j := 0; j < b.height; j ++ {
+		if b.returnTile(j, 0).value == 'X' {
+
+			queue= append(queue, b.returnTile(j, 0))
+		}
+	}
+
+	for _, t := range queue {
+
+		if isWinner(b, t) {
+			return true
+		}
+		winner = depthFirstSearch(b, t)
+	}
+
+
+	return
+}
+
+func checkY (b Board) (winner bool) {
+
+	// bottom to top
+
+	var queue []*Tile
+
+	for j := 0; j < b.width; j ++ {
+		if b.returnTile(0, j).value == 'O' {
+
+			queue= append(queue, b.returnTile(0, j))
+		}
+	}
+
+	for _, t := range queue {
+		if isWinner(b, t) {
+			return true
+		}
+		winner = depthFirstSearch(b, t)
+	}
+
+
+	return
+
+}
+
+func depthFirstSearch (b Board, t *Tile) bool {
+
+	curr := t.headP
+
+	t.checked = true
+
+	if curr == nil {
+		return false
+	} else {
+		for curr != nil {
+			if isWinner(b, curr.destination) {
+				return true
+			}
+			if !curr.destination.checked {
+				return depthFirstSearch(b, curr.destination)
+			}
+			curr = curr.nextP
+		}
+	}
+
+	return false
+}
+
+func isWinner(b Board, t *Tile) bool {
+
+	if t.value == 'X' {
+		if t.col + 1 == b.width {
+			return true
+		}
+	} else if t.value == 'O' {
+		if t.row + 1 == b.height {
+			return true
+		}
+	}
+
+	return false
+}
 
 
 
 
-func ResultOf (sboard []string) (bool,error) {
+func ResultOf (sboard []string) (string,error) {
 
 	//tile := NewTile(1,1, 'X')
 	board := NewBoard(sboard)
@@ -209,12 +307,12 @@ func ResultOf (sboard []string) (bool,error) {
 	//tile.printTile()
 	//board.printBoard()
 
-	//board.returnTile(2,2).printTile()
+	//board.returnTile(0,0).printTile()
 
 	initGraph(board)
-	board.printBoard()
+	//board.printBoard()
 
+	winner := board.checkWinner()
 
-
-	return true, nil
+	return winner, nil
 }
